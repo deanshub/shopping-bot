330e4dea03970b0babfde4851bb366228f801350
Dean Shub <deanshub@gmail.com>
2025-10-06 13:39:06 +0300

    refactor

--- shoppingList.ts
+++ shoppingList.ts
@@ -1,71 +1,85 @@
-import fs from "node:fs/promises"
 import { SHOPPING_LISTS_DIR } from "./consts";
-import { mkdir } from 'node:fs/promises'
+import { mkdir, readdir, readFile, writeFile } from "node:fs/promises";
 
-
-const shoppingLists = new Map<number, string[]>()
+const shoppingLists = new Map<number, string[]>();
 
 export function getItemsFromText(text: string): string[] {
-    return text.split("\n")
-        .map((item) => item.trim())
-        .map((item) => {
-            const match = item.match(/^(\d+)/);
-            if (match) {
-                return `${item.slice(match[0].length).trim()} ${match[0]}`
-            }
-            return item;
-        })
+  return text
+    .split("\n")
+    .map((item) => item.trim())
+    .map((item) => {
+      const match = item.match(/^(\d+)/);
+      if (match) {
+        return `${item.slice(match[0].length).trim()} ${match[0]}`;
+      }
+      return item;
+    });
 }
 
-export async function loadShoppingLists(){
-    const files = await fs.readdir(SHOPPING_LISTS_DIR)
-    for (const file of files) {
-        const shoppingList = await fs.readFile(`${SHOPPING_LISTS_DIR}/${file}`, "utf-8")
-        shoppingLists.set(parseInt(file.split(".")[0]), JSON.parse(shoppingList))
-    }
+export async function loadShoppingLists() {
+  const files = await readdir(SHOPPING_LISTS_DIR);
+  for (const file of files) {
+    const shoppingList = await readFile(
+      `${SHOPPING_LISTS_DIR}/${file}`,
+      "utf-8"
+    );
+    shoppingLists.set(parseInt(file.split(".")[0]), JSON.parse(shoppingList));
+  }
 }
 
 export async function getShoppingList(listId: number): Promise<string[]> {
-    try {
-        const shoppingList = shoppingLists.get(listId)
-        return shoppingList ?? []
-    } catch (error) {
-        shoppingLists.set(listId, [])
-    }
-    return []
+  try {
+    const shoppingList = shoppingLists.get(listId);
+    return shoppingList ?? [];
+  } catch (error) {
+    shoppingLists.set(listId, []);
+  }
+  return [];
 }
 
 export async function addItemsToShoppingList(items: string[], listId: number) {
-    const originalShoppingList = await getShoppingList(listId)
-    const itemsToRemove = items.filter((item) => item.includes('לא צריך') || item.includes('קניתי'))
-    const newItems = items.filter((item) => !originalShoppingList.includes(item) && !itemsToRemove.includes(item))
-    const renamedItemsToRemove = itemsToRemove.map((item) => {
-        return item.replace('לא צריך', '').replace('קניתי', '').trim()
-    })
-    const shoppingList = originalShoppingList.filter((item) => {
-        // return !renamedItemsToRemove.includes(item)
-        return !renamedItemsToRemove.some(removeItem => item.includes(removeItem))
-    })
+  const originalShoppingList = await getShoppingList(listId);
+  const itemsToRemove = items.filter(
+    (item) => item.includes("לא צריך") || item.includes("קניתי")
+  );
+  const newItems = items.filter(
+    (item) =>
+      !originalShoppingList.includes(item) && !itemsToRemove.includes(item)
+  );
+  const renamedItemsToRemove = itemsToRemove.map((item) => {
+    return item.replace("לא צריך", "").replace("קניתי", "").trim();
+  });
+  const shoppingList = originalShoppingList.filter((item) => {
+    // return !renamedItemsToRemove.includes(item)
+    return !renamedItemsToRemove.some((removeItem) =>
+      item.includes(removeItem)
+    );
+  });
 
-    
-    const newShoppingList = [...shoppingList, ...newItems];
-    shoppingLists.set(listId, newShoppingList)
-    await fs.writeFile(`${SHOPPING_LISTS_DIR}/${listId}.json`, JSON.stringify(newShoppingList, null, 2))
+  const newShoppingList = [...shoppingList, ...newItems];
+  shoppingLists.set(listId, newShoppingList);
+  await writeFile(
+    `${SHOPPING_LISTS_DIR}/${listId}.json`,
+    JSON.stringify(newShoppingList, null, 2)
+  );
 }
 
 export async function clearList(listId: number) {
-    shoppingLists.set(listId, [])
-    await fs.writeFile(`${SHOPPING_LISTS_DIR}/${listId}.json`, JSON.stringify([], null, 2))
+  shoppingLists.set(listId, []);
+  await writeFile(
+    `${SHOPPING_LISTS_DIR}/${listId}.json`,
+    JSON.stringify([], null, 2)
+  );
 }
 
 export function getAllShoppingLists(): Map<number, string[]> {
-    return shoppingLists
+  return shoppingLists;
 }
 
-export async function ensureShoppingListDir(){
-    try {
-        await mkdir(SHOPPING_LISTS_DIR, { recursive: true })
-    } catch (error) {
-        console.error(error)
-    }
+export async function ensureShoppingListDir() {
+  try {
+    await mkdir(SHOPPING_LISTS_DIR, { recursive: true });
+  } catch (error) {
+    console.error(error);
+  }
 }
